49ccb4b4b2b184eb49d44ab490554484770e9626
Shifat507 <ferdoushasanshifat@gmail.com>
2024-11-26 19:15:42 +0600

    name validation

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,10 +1,11 @@
-import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useContext, useState } from 'react';
+import { Link, Navigate, useNavigate } from 'react-router-dom';
 import { authContext } from '../provider/AuthProvider';
 
 const Register = () => {
     const { handleRegister, setUser } = useContext(authContext);
-
+    const [error, setError] = useState({})
+    const navigate = useNavigate();
     const handleSubmitRegister = (e) => {
         e.preventDefault();
 
@@ -13,7 +14,11 @@ const Register = () => {
         const email = e.target.email.value;
         const password = e.target.password.value;
 
-     
+        if(name.length < 5){
+            setError({...error, name:"Must me more than 5 character or long"});
+            return;
+        }
+
         handleRegister(email, password)
             .then((res) => {
                 const user = res.user;
@@ -23,7 +28,8 @@ const Register = () => {
                 user.photoURL = photoUrl;
 
                 setUser(user);
-                console.log('Registered User:', user.email);
+                navigate('/');
+                // console.log('Registered User:', user.email);
             })
             .catch((error) => {
                 console.error('Registration Error:', error.message);
@@ -45,6 +51,11 @@ const Register = () => {
                             className="input input-bordered"
                             required
                         />
+                        {
+                            error.name && (<label className="label text-xs text-red-600">
+                                {error.name}
+                            </label>)
+                        }
                     </div>
                     <div className="form-control">
                         <label className="label">
